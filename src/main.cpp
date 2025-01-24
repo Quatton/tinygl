d6062b51e0ae4c7ee11b429aad5deced3eaaa0b2
Quatton <atomicatives@gmail.com>
2025-01-24 13:22:21 +0900

    triangle: part 2 (rect)

--- src/main.cpp
+++ src/main.cpp
@@ -98,9 +98,15 @@ int main() {
   glDeleteShader(fragmentShader);
 
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f,  // left
-      0.5f,  -0.5f, 0.0f,  // right
-      0.0f,  0.5f,  0.0f   // top
+      0.5f,  0.5f,  0.0f,  // top right
+      0.5f,  -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f,  // bottom left
+      -0.5f, 0.5f,  0.0f   // top left
+  };
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 3,  // first triangle
+      1, 2, 3   // second triangle
   };
 
   unsigned int VAO;
@@ -110,8 +116,15 @@ int main() {
   unsigned int VBO;
   glGenBuffers(1, &VBO);
 
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
@@ -127,7 +140,7 @@ int main() {
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
